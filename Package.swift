f8cd0d23037ddba6da15b85b81c72eda36d8280f
Joannis Orlandos <joannis@orlandos.nl>
2016-04-06 11:06:28 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "CryptoKitten",
     dependencies: [
-                      .Package(url: "https://github.com/SwiftX/C7.git", majorVersion: 0, minor: 1),
+                      .Package(url: "https://github.com/SwiftX/C7.git", majorVersion: 0, minor: 2),
                       ]
 )
 
